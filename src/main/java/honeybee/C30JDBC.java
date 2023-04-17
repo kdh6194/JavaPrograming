438bfdb0c87e60176d26cd487dad34d824c976b8
kdh6194 <kdh6194@naver.com>
2023-04-17 10:50:18 +0900

    도서 정보 adv select - update

---
+++ src/main/java/honeybee/C30JDBC.java
@@ -0,0 +1,131 @@
+package honeybee;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class C30JDBC {
+    private static String DRV = "org.mariadb.jdbc.Driver";
+    private static String URL = "jdbc:mariadb://fullstacks.cclckhepzkvp.ap-northeast-2.rds.amazonaws.com:3306/fulstacks";
+    private static String USR = "admin";
+    private static  String PWD = "fullstack_2023";
+
+    private static String updateBookSQL = "update newbooks set title = ? , writer = ? , price = ? where bookno = ?";
+
+
+    public static void main(String[] args) {
+        // newbooks 테이블에서 입력받은
+        // 도서정보(도서번호, 도서명, 저자, 가격)를
+        // 이용해서 레코드 수정
+
+        // 수정할 도서번호 입력받음
+        Scanner sc = new Scanner(System.in);
+        System.out.println("수정할 도서번호는?");
+        int bookno = sc.nextInt();
+        System.out.println("수정할 도서이름은?");
+        String title = sc.next();
+        System.out.println("수정할 저자는?");
+        String writer = sc.next();
+        System.out.println("수정할 가격은?");
+        int price = sc.nextInt();
+
+
+        try {
+          Class.forName(DRV);
+        } catch (Exception e) {
+            System.out.println("mariadb 용 JDBC 드라이버가 없어요!");
+        }
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+
+        try {
+            conn = DriverManager.getConnection(URL,USR,PWD);
+            pstmt = conn.prepareStatement(updateBookSQL);
+            // 두번째 인자는 데이터베이스에 컬럼명이 아니라 입력받는 변수명을 받아와야한다.
+            // 그리고 ? 순서에 따라서 값을 넣어줘야 한다. 입력받는 순서가 아니다.
+            pstmt.setString(1,title);
+            pstmt.setString(2,writer);
+            pstmt.setInt(3,price);
+            pstmt.setInt(4,bookno);
+
+
+            // SQL문 실행 후 결과 확인
+            int del = pstmt.executeUpdate();  // DML 실행시 사용
+            System.out.println("데이터 수정확인 : "+ del);
+
+
+        } catch (SQLException e) {
+            System.out.println("db 주소나 아이디/비밀번호를 확인하세요");
+        }finally {
+            if (pstmt != null) try{pstmt.close();} catch(Exception ex){}
+            if (conn != null) try{conn.close();} catch(Exception ex){}
+        }
+
+
+    }
+}
+class Book4 {
+    private int bookno;
+    private String title;
+    private String writer;
+    private int price;
+    private String regdate;
+    public Book4() {}
+    public Book4(int bookno, String title, String writer, int price, String regdate) {
+        this.bookno = bookno;
+        this.title = title;
+        this.writer = writer;
+        this.price = price;
+        this.regdate = regdate;
+    }
+
+    public int getBookno() {
+        return bookno;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getRegdate() {
+        return regdate;
+    }
+
+    public void setBookno(int bookno) {
+        this.bookno = bookno;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setRegdate(String regdate) {
+        this.regdate = regdate;
+    }
+
+    @Override
+    public String toString() {
+        String fmt = "%d %s %s %d %s";
+        return String.format(fmt,bookno,title,writer,price,regdate);
+    }
+}
